a6bfbc279d244a89a1cae4f68f7ee0f438397cd7
Henok Samson <henoksamson57@gmail.com>
2024-09-01 16:42:47 +0300

    Revert "Feature/12 build user authentication and profile UI frontend"

--- backend/src/config/database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  logging: false, // Disable logging; default: console.log
-});
-
-export default sequelize;
